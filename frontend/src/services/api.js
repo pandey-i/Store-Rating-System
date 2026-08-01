7384790c595fbce04ddd2186da2130f27c9b2b93
Aman Pandey <amanpandey94158@gmail.com>
2026-08-01 16:38:10 +0530

    commit prisma&api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://store-rating-api-u1fv.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
